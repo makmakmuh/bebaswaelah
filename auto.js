050c300a958b641d326a699c17633dbdac5be7af
makmakmuh <92998008+makmakmuh@users.noreply.github.com>
2021-10-25 20:38:27 +0700

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,1 +1,13 @@
 
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2051909233534701"
+     crossorigin="anonymous"></script>
+<!-- Ads Display 1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2051909233534701"
+     data-ad-slot="1976157655"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
